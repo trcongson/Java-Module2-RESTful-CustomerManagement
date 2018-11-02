e7f3ee1ba57b210c4af79517b6db06684143b812
trcongson <trcongson.tcs@gmail.com>
2018-11-02 15:00:35 +0700

    1st cm

---
+++ src/main/java/com/codegym/cms/controller/CustomerController.java
@@ -0,0 +1,48 @@
+package com.codegym.cms.controller;
+
+import com.codegym.cms.model.Customer;
+import com.codegym.cms.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+@RestController
+public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @RequestMapping(value = "/customer/", method = RequestMethod.GET)
+    public ResponseEntity<List<Customer>>listAllCustomer(){
+        List<Customer> customers = customerService.findAll();
+        if (customers.isEmpty()){
+            return new ResponseEntity<List<Customer>>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<List<Customer>>(customers,HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/customer/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Customer> getCustomer(@PathVariable("id")long id){
+        System.out.println("Fetching Customer with id " + id);
+        Customer customer = customerService.findById(id);
+        if (customer == null){
+            System.out.println("Customer with id" + id + " not found");
+            return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Customer>(customer, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/customers/", method = RequestMethod.POST)
+    public ResponseEntity<Void> createCustomer(@RequestBody Customer customer, UriComponentsBuilder ucBuilder){
+        System.out.println("Creating Customer " + customer.getLastName());
+        customerService.save(customer);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ucBuilder.path("/customers/{id}").buildAndExpand(customer.getId()).toUri());
+        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+    }
+}
